5726ce0a5c0d0ede40b2930c462c18a16b7c30e2
Ian Roberts <thorshammer01889@gmail.com>
2013-02-05 01:16:58 -0500

    it sets the firing arc but it is ignored might be always read from blueprint and thus global?

--- Rose_Com (sent)/hook/units/UEB2303/UEB2303_Script.lua
+++ Rose_Com (sent)/hook/units/UEB2303/UEB2303_Script.lua
@@ -4,15 +4,18 @@ UEB2303 = Class(oldUEB2303) {
 	
 	
 	OnScriptBitSet = function(self, bit)
-
         if bit == 1 then 
 			self:GetWeaponByLabel('MainGun').BallisticArc = 'RULEUBA_HighArc'
+			self:GetWeaponByLabel('MainGun'):SetupTurret()
+			LOG(self:GetWeaponByLabel('MainGun').BallisticArc)
         end
     end,
 	
 	OnScriptBitClear = function(self, bit)
         if bit == 1 then 
 			self:GetWeaponByLabel('MainGun').BallisticArc = 'RULEUBA_LowArc'
+			self:GetWeaponByLabel('MainGun'):SetupTurret()
+			LOG(self:GetWeaponByLabel('MainGun').BallisticArc)
         end
     end,
 }
