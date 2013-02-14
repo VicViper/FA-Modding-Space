aa72d07fba0ca9f432a651b2e313be765e876cf4
Ian Roberts <thorshammer01889@gmail.com>
2013-02-13 20:16:08 -0500

    change layouts to the new hover icons

--- Rose_Com (sent)/hook/textures/ui/common/game/strategicicons/strategicicons,hover_over_layout.lua
+++ Rose_Com (sent)/hook/textures/ui/common/game/strategicicons/strategicicons,hover_over_layout.lua
@@ -1,6 +1,6 @@
 layout = {
-	['icon_gunship_antiair_over'] = {left = 8, top = 11, width = 16, height = 12, },
-	['icon_gunship_transport_over'] = {left = 8, top = 11, width = 16, height = 12, },
-	['icon_gunship_directfire_over'] = {left = 8, top = 11, width = 16, height = 12, },
-	['icon_gunship_generic_over'] = {left = 8, top = 11, width = 16, height = 12, },
+	['icon_hover_antiair_over'] = {left = 8, top = 11, width = 16, height = 12, },
+	['icon_hover_transport_over'] = {left = 8, top = 11, width = 16, height = 12, },
+	['icon_hover_directfire_over'] = {left = 8, top = 11, width = 16, height = 12, },
+	['icon_hover_generic_over'] = {left = 8, top = 11, width = 16, height = 12, },
 }
