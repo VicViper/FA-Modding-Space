91a683def862193eeaa448c3b9a3447ef96f5f99
Vic Viper <LightningBruiser@gmail.com>
2013-03-20 19:29:22 -0500

    Added icons for armoured assault bots, and anti-shield unts. Additionally fixed some hover unit icons. (Second attempt to commit)

--- Rose_Com (sent)/shadow/textures/ui/common/game/strategicicons/strategicicons,hover3_rest_layout.lua
+++ Rose_Com (sent)/shadow/textures/ui/common/game/strategicicons/strategicicons,hover3_rest_layout.lua
@@ -1,5 +1,6 @@
 layout = {
-	['icon_hover3_antiair_rest'] = {left = 8, top = 11, width = 16, height = 12, },
+	['icon_hover3_antishield_rest'] = {left = 8, top = 11, width = 16, height = 12, },
+  ['icon_hover3_antiair_rest'] = {left = 8, top = 11, width = 16, height = 12, },
 	['icon_hover3_transport_rest'] = {left = 8, top = 11, width = 16, height = 12, },
 	['icon_hover3_directfire_rest'] = {left = 8, top = 11, width = 16, height = 12, },
 	['icon_hover3_generic_rest'] = {left = 8, top = 11, width = 16, height = 12, },
